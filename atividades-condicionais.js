291327db12435d3a5ea8ec158c7087764e3272ec
Duarte <duarte.desousa.contato@gmail.com>
2026-02-23 14:26:02 -0300

    first commit

---
+++ atividades-condicionais.js
@@ -0,0 +1,164 @@
+// Atv1
+let num = 10;
+if (num > 0) {
+    console.log("Positivo");
+} else if (num < 0){
+    console.log("Negativo");
+} else{
+    console.log("Nulo");
+}
+console.log("");
+
+//---------------------------------------
+// Atv2
+let idade = 17;
+if (idade >= 18) {
+    console.log("Maior de idade");
+} else{
+    console.log("Menor de idade");
+}
+console.log("");
+
+//---------------------------------------
+// Atv3
+let numII = 17;
+if (numII % 2 == 1) {
+    console.log("Ímpar");
+} else{
+    console.log("Par");
+}
+console.log("");
+
+//---------------------------------------
+// Atv4
+let nota = 8;
+if (nota >= 6) {
+    console.log("Aprovado")
+} else{
+    console.log("Reprovado")
+}
+console.log("");
+
+//---------------------------------------
+// Atv5
+idade = 19;
+if (idade >= 16) {
+    console.log("Pode votar");
+} else{
+    console.log("Não pode votar");
+}
+console.log("");
+
+//---------------------------------------
+// Atv6
+let senha = "1235";
+if (senha === "1234") {
+    console.log("Acesso permitido");
+} else{
+    console.log("Acesso negado");
+}
+console.log("")
+
+//---------------------------------------
+// Atv7
+let temperatura = -30;
+if (temperatura >= 25) {
+    console.log("Está quente");
+} else{
+    console.log("Está frio");
+}
+console.log("")
+
+//---------------------------------------
+// Atv8
+let alturamin = 1.39;
+if (alturamin >= 1.40) {
+    console.log("Pode entrar")
+} else{
+    console.log("Não pode entrar")
+}
+console.log("")
+
+//---------------------------------------
+// Atv9
+num = 0;
+if (num > 0) {
+    console.log("Positivo");
+} else if (num < 0){
+    console.log("Negativo");
+} else{
+    console.log("Zero");
+}
+console.log("");
+
+//---------------------------------------
+// Atv10
+nota = 7;
+if (nota >= 9) {
+    console.log("Excelente");
+} else if (nota == 7 || nota == 8){
+    console.log("Bom");
+} else if (nota == 6){
+    console.log("Regular");
+} else{
+    console.log("Reprovado");
+}
+console.log("");
+
+//---------------------------------------
+// Atv11
+idade = 19;
+let temCNH = false;
+if (idade >=18 && temCNH) {
+    console.log("Pode dirigir");
+} else{
+    console.log("Não pode dirigir");
+}
+console.log("");
+
+
+//---------------------------------------
+// Atv12
+compra = 120;
+if (compra > 100) {
+    console.log("Ganhou um desconto");
+} else{
+    console.log("Não há descontos");
+}
+console.log("");
+
+//---------------------------------------
+// Atv13
+let usuario = "cliente";
+senha = "123";
+
+if (usuario == "admin" && senha === "123") {
+    console.log("Login realizado");
+} else{
+    console.log("Erro de login");
+}
+console.log("");
+
+//---------------------------------------
+// Atv14
+idade = 11;
+if (idade > 16) {
+    console.log("Pode assistir");
+} else{
+    console.log("Não pode assistir");
+}
+console.log("");
+
+//---------------------------------------
+// Atv15
+let cor = "amarelo";
+if (cor == "verde") {
+    console.log("Pode passar");
+} else if (cor == "amarelo"){
+    console.log("Atenção");
+} else if (cor == "vermelho"){
+    console.log("Pare");
+} else {
+    console.log("Semáforo quebrado");
+}
+console.log("");
